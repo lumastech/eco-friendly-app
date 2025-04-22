eef911190e8af5c903063432a27d2d42e43146ad
lumas <lumastech@gmail.com>
2025-04-22 08:21:11 +0200

    created and designed course list item

---
+++ app/src/main/java/com/lumastech/ecoapp/Models/Course.java
@@ -0,0 +1,10 @@
+package com.lumastech.ecoapp.Models;
+
+
+public class Course {
+    public long id;
+    public String name;
+    public String description;
+    public String coordinator;
+    public String status;
+}
