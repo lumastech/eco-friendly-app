d24f8867958b506fecd59b0140d8c84b4db73181
lumas <lumastech@gmail.com>
2025-04-22 08:36:22 +0200

    added course fragment

---
+++ app/src/main/java/com/lumastech/ecoapp/Course/CourseFragment.java
@@ -0,0 +1,42 @@
+package com.lumastech.ecoapp.Course;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lumastech.ecoapp.R;
+
+
+public class CourseFragment extends Fragment {
+
+    public CourseFragment() {
+        // Required empty public constructor
+    }
+
+    public static CourseFragment newInstance(String param1, String param2) {
+        return new CourseFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_course, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
