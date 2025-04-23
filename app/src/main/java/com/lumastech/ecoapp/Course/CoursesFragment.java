db47149863c6bdaaacd5609543ae1df54a02c265
Lumas <lumastech@gmail.com>
2025-04-23 17:07:51 +0200

    updated course fragment

--- app/src/main/java/com/lumastech/ecoapp/Course/CoursesFragment.java
+++ app/src/main/java/com/lumastech/ecoapp/Course/CoursesFragment.java
@@ -10,17 +10,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.lumastech.ecoapp.Models.Course;
 import com.lumastech.ecoapp.R;
+import com.lumastech.ecoapp.Utility;
+
+import java.util.List;
 
 
-public class CourseFragment extends Fragment {
+public class CoursesFragment extends Fragment {
 
-    public CourseFragment() {
+    private List<Course> itemList;
+    private Utility utility;
+    public CoursesFragment() {
         // Required empty public constructor
     }
 
-    public static CourseFragment newInstance(String param1, String param2) {
-        return new CourseFragment();
+    public static CoursesFragment newInstance(String param1, String param2) {
+        return new CoursesFragment();
     }
 
     @Override
@@ -32,7 +38,7 @@ public class CourseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course, container, false);
+        return inflater.inflate(R.layout.fragment_courses, container, false);
     }
 
     @Override
