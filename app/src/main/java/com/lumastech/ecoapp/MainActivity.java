7edb0d9bba5d5513637b0fc2e9368b95fb8f237f
lumas <lumastech@gmail.com>
2025-04-24 08:10:28 +0200

    implemented MainFragment

--- app/src/main/java/com/lumastech/ecoapp/MainActivity.java
+++ app/src/main/java/com/lumastech/ecoapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.lumastech.ecoapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,10 +21,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.lumastech.ecoapp.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavListener {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private Utility utility;
+    private Context context;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        context = getApplicationContext();
 
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
@@ -45,10 +54,13 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_home)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        utility = new Utility(getApplicationContext());
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,8 +71,35 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    @Override
+    public void onButtonClicked(int id) {
+        navController.navigate(id);
+    }
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.main, menu); // Inflate the menu
+        super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            utility.logout();
+            startActivity(new Intent(context, LoginActivity.class));
+            finish();
+            return true;
+        }
+
+        if (item.getItemId() == R.id.action_sync) {
+            //dataSync.refresh();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
