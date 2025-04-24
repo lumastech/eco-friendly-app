14bb12612d861006aa929662cb60f876b76b3e4c
lumas <lumastech@gmail.com>
2025-04-24 08:09:42 +0200

    implemented CoursesFragment

--- app/src/main/java/com/lumastech/ecoapp/Course/CoursesFragment.java
+++ app/src/main/java/com/lumastech/ecoapp/Course/CoursesFragment.java
@@ -1,25 +1,33 @@
 package com.lumastech.ecoapp.Course;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.lumastech.ecoapp.Models.Course;
 import com.lumastech.ecoapp.R;
 import com.lumastech.ecoapp.Utility;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class CoursesFragment extends Fragment {
 
     private List<Course> itemList;
+    private CourseAdapter adapter;
+    private RecyclerView recyclerView;
+    private Context context;
     private Utility utility;
     public CoursesFragment() {
         // Required empty public constructor
@@ -44,5 +52,29 @@ public class CoursesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+        utility = new Utility(context);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        adapter = new CourseAdapter(getDummyData(), new CourseAdapter.SelectItem() {
+            @Override
+            public void onItemClicked(Course item) {
+                Toast.makeText(context, "Course", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    public ArrayList<Course> getDummyData(){
+        ArrayList<Course> list = new ArrayList<>();
+        list.add(new Course( 1, "Mathematics", "An introduction to mathematical concepts.", "John Doe", "Active" ));
+        list.add(new Course( 2, "Physics", "Fundamentals of physics", "Jane Smith", "Active" ));
+        list.add(new Course(3, "Chemistry", "Core concepts in chemistry", "Bob Johnson", "Inactive"));
+        list.add(new Course(4, "Biology", "Introduction to biological systems", "Alice Brown", "Active"));
+        list.add(new Course(5, "History", "World history overview", "Eve Davis", "Inactive"));
+        return  list;
     }
 }
