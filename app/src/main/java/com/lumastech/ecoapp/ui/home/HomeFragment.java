a3a0263d06d59f43ccbd6e5d60f3d8a00782a0b0
lumas <lumastech@gmail.com>
2025-04-24 08:10:05 +0200

    implemented HomeFragment

--- app/src/main/java/com/lumastech/ecoapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/lumastech/ecoapp/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.lumastech.ecoapp.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,21 +8,50 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.lumastech.ecoapp.NavListener;
 import com.lumastech.ecoapp.R;
 import com.lumastech.ecoapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
-
+    private View viewMoreCourses;
+    private NavListener listener;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewMoreCourses = view.findViewById(R.id.view_more_courses);
+
+        viewMoreCourses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null){
+                    listener.onButtonClicked(R.id.nav_fragment_courses);
+                }
+            }
+        });
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof NavListener) {
+            listener = (NavListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
     }
 }
