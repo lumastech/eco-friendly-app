ef93e80e31af41dc2318281ef16f913821c4631b
lumas <lumastech@gmail.com>
2025-04-26 18:19:50 +0300

    updated created Lesson data model class

---
+++ app/src/main/java/com/lumastech/ecoapp/Models/Lesson.java
@@ -0,0 +1,21 @@
+package com.lumastech.ecoapp.Models;
+
+public class Lesson {
+    public  long id;
+    public String title;
+    public String description;
+    public String content;
+    public String duration;
+    public String difficulty;
+    public int order;
+    public String video_url;
+    public float points;
+    public  String infographic_url;
+    public boolean is_free;
+
+    public Lesson(long id, String title, float points){
+        this.id = id;
+        this.title = title;
+        this.points = points;
+    }
+}
