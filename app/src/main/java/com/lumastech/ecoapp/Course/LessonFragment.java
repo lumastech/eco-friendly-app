d71d95c97b44568a7b400f599694c3cf89caff3c
lumas <lumastech@gmail.com>
2025-04-26 18:21:04 +0300

    implemented lesson fragment class

--- app/src/main/java/com/lumastech/ecoapp/Course/LessonFragment.java
+++ app/src/main/java/com/lumastech/ecoapp/Course/LessonFragment.java
@@ -1,60 +1,52 @@
 package com.lumastech.ecoapp.Course;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.lumastech.ecoapp.Models.Course;
+import com.lumastech.ecoapp.Models.Lesson;
+import com.lumastech.ecoapp.NavListener;
 import com.lumastech.ecoapp.R;
+import com.lumastech.ecoapp.Utility;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LessonFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class LessonFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private Context context;
+    private Utility utility;
+    private NavListener listener;
+    TextView lessonCounter, title, content;
+    ImageView imageView;
+    Lesson lesson;
+    Button takeTestBtn;
     public LessonFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LessonFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LessonFragment newInstance(String param1, String param2) {
-        LessonFragment fragment = new LessonFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public static LessonFragment newInstance() {
+        return new LessonFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -63,4 +55,47 @@ public class LessonFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lesson, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+        utility = new Utility(context);
+        lessonCounter = view.findViewById(R.id.lesson_counter);
+        title = view.findViewById(R.id.lesson_title);
+        content = view.findViewById(R.id.content);
+        imageView = view.findViewById(R.id.imageView);
+        lesson = Utility.LESSON;
+        takeTestBtn = view.findViewById(R.id.take_test_btn);
+
+        if(lesson != null) {
+            title.setText(lesson.title);
+            lessonCounter.setText("LESSON ");
+            content.setText("Content");
+        }
+
+        takeTestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null){
+                    listener.onButtonClicked(R.id.nav_fragment_quiz);
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof NavListener) {
+            listener = (NavListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
 }
+
+
