67be1456c7f2532d5e0c9731b8fa711f3c6e1256
Lumas <lumastech@gmail.com>
2025-04-28 12:45:35 +0200

    updated Course model

--- app/src/main/java/com/lumastech/ecoapp/Models/Course.java
+++ app/src/main/java/com/lumastech/ecoapp/Models/Course.java
@@ -1,18 +1,22 @@
 package com.lumastech.ecoapp.Models;
 
 
+import java.util.List;
+
 public class Course {
     public long id;
     public String name;
     public String description;
     public String coordinator;
     public String status;
+    public List<Lesson> lessons;
 
-    public Course(long id, String name, String description, String coordinator, String status) {
+    public Course(long id, String name, String description, String coordinator, String status, List<Lesson> lessons) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.coordinator = coordinator;
         this.status = status;
+        this.lessons = lessons;
     }
 }
