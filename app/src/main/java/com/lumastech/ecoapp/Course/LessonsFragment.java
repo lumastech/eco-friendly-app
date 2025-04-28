554e7ebb021aa1ce13b004a189dacf417e8cf66b
lumas <lumastech@gmail.com>
2025-04-28 08:23:00 +0300

    updated LessonsFragment class

--- app/src/main/java/com/lumastech/ecoapp/Course/LessonsFragment.java
+++ app/src/main/java/com/lumastech/ecoapp/Course/LessonsFragment.java
@@ -12,7 +12,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.lumastech.ecoapp.Models.Course;
 import com.lumastech.ecoapp.Models.Lesson;
