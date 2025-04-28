6acd525971b7be15979cb2fd8b1db00674196a93
Lumas <lumastech@gmail.com>
2025-04-28 12:45:44 +0200

    updated Lesson model

--- app/src/main/java/com/lumastech/ecoapp/Models/Lesson.java
+++ app/src/main/java/com/lumastech/ecoapp/Models/Lesson.java
@@ -1,5 +1,7 @@
 package com.lumastech.ecoapp.Models;
 
+import java.util.List;
+
 public class Lesson {
     public  long id;
     public String title;
@@ -11,11 +13,15 @@ public class Lesson {
     public String video_url;
     public float points;
     public  String infographic_url;
+    public List<Quiz> quizzes;
     public boolean is_free;
 
-    public Lesson(long id, String title, float points){
+    public Lesson(long id, String title, float points, String description, String content, List<Quiz> quizzes){
         this.id = id;
         this.title = title;
         this.points = points;
+        this.description = description;
+        this.content = content;
+        this.quizzes = quizzes;
     }
 }
