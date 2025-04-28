ef2cfdfb2be3a4cbec6502494dfcb5f6e616085a
lumas <lumastech@gmail.com>
2025-04-28 08:19:00 +0300

    implemented forum fragment class

--- app/src/main/java/com/lumastech/ecoapp/Forum/ForumFragment.java
+++ app/src/main/java/com/lumastech/ecoapp/Forum/ForumFragment.java
@@ -1,60 +1,50 @@
 package com.lumastech.ecoapp.Forum;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.lumastech.ecoapp.Models.Comment;
+import com.lumastech.ecoapp.Models.Post;
 import com.lumastech.ecoapp.R;
+import com.lumastech.ecoapp.Utility;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ForumFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ForumFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Context context;
+    private TextView title, content;
+    private ImageView imageView;
+    private Post item;
+    private RecyclerView recyclerView;
+    private CommentAdapter commentAdapter;
+    private Button commentBtn;
+    private EditText commentEt;
 
     public ForumFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForumFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ForumFragment newInstance(String param1, String param2) {
-        ForumFragment fragment = new ForumFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return new ForumFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +52,52 @@ public class ForumFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_forum, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+        title = view.findViewById(R.id.post_title);
+        content = view.findViewById(R.id.post_content);
+        imageView = view.findViewById(R.id.imageView);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        commentBtn = view.findViewById(R.id.comment_btn);
+        commentEt = view.findViewById(R.id.comment_edit);
+
+        item = Utility.POST;
+
+        if (item != null){
+            title.setText(item.title);
+            content.setText(item.content);
+        }
+
+        commentAdapter = new CommentAdapter(item.comments, new CommentAdapter.SelectItem() {
+            @Override
+            public void onItemClicked(Comment item) {
+
+            }
+        });
+
+        commentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String comment = commentEt.getText().toString().trim();
+                if (comment.isEmpty()){
+                    commentEt.setError("Please Type Your comment");
+                    return;
+                }
+                postComment(comment);
+            }
+        });
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(commentAdapter);
+    }
+
+    private void postComment(String comment) {
+        // post comment
+        Toast.makeText(context, "post submitted", Toast.LENGTH_SHORT).show();
+        commentEt.setText("");
     }
 }
