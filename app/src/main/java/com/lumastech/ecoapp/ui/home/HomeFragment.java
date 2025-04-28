43545db65fc9c297aaabaea7e67453eb47629b0c
lumas <lumastech@gmail.com>
2025-04-28 08:22:30 +0300

    updated homeFragment class

--- app/src/main/java/com/lumastech/ecoapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/lumastech/ecoapp/ui/home/HomeFragment.java
@@ -5,19 +5,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.lumastech.ecoapp.Course.CourseAdapter;
+import com.lumastech.ecoapp.Course.CoursesFragment;
+import com.lumastech.ecoapp.Models.Course;
 import com.lumastech.ecoapp.NavListener;
 import com.lumastech.ecoapp.R;
+import com.lumastech.ecoapp.Utility;
 import com.lumastech.ecoapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
     private View viewMoreCourses;
+    private Button continueLearning, forumBtn;
     private NavListener listener;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +36,8 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         viewMoreCourses = view.findViewById(R.id.view_more_courses);
+        continueLearning = view.findViewById(R.id.continue_learning);
+        forumBtn = view.findViewById(R.id.forums_btn);
 
         viewMoreCourses.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +47,29 @@ public class HomeFragment extends Fragment {
                 }
             }
         });
+        continueLearning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null){
+                    listener.onButtonClicked(R.id.nav_fragment_courses);
+                }
+            }
+        });
+
+        forumBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null){
+                    listener.onButtonClicked(R.id.nav_fragment_forums);
+                }
+            }
+        });
+
+
+
+
+
+        setModules(view);
     }
 
     @Override
@@ -53,5 +86,23 @@ public class HomeFragment extends Fragment {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
+    }
+
+    public void setModules(View view){
+        CoursesFragment coursesFragment = new CoursesFragment();
+        RecyclerView recyclerView = view.findViewById(R.id.module_recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        CourseAdapter adapter = new CourseAdapter(coursesFragment.getDummyData(), new CourseAdapter.SelectItem() {
+            @Override
+            public void onItemClicked(Course item) {
+                if (listener != null){
+                    Utility.COURSE = item;
+                    listener.onButtonClicked(R.id.nav_fragment_lessons);
+                };
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
     }
 }
