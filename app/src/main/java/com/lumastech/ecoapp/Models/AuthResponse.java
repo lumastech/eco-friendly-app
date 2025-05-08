43f45cbc4934bda033105e4efe7710dfeed7a73d
lumas <lumastech@gmail.com>
2025-05-09 00:15:07 +0300

    implemented AuthResponse class

---
+++ app/src/main/java/com/lumastech/ecoapp/Models/AuthResponse.java
@@ -0,0 +1,65 @@
+package com.lumastech.ecoapp.Models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AuthResponse {
+    @SerializedName("access_token")
+    private String accessToken;
+
+    @SerializedName("token_type")
+    private String tokenType;
+
+    @SerializedName("expires_in")
+    private long expiresIn;
+
+    @SerializedName("refresh_token")
+    private String refreshToken;
+
+    private User user;
+
+    // Getters and Setters
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    // Helper method to get full authorization token
+    public String getAuthorizationHeader() {
+        return tokenType + " " + accessToken;
+    }
+}
