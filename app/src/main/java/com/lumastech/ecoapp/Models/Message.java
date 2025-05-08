58d46986584ae628c9f23cc20a83689faefb11d2
lumas <lumastech@gmail.com>
2025-05-09 00:19:55 +0300

    added receiver id

--- app/src/main/java/com/lumastech/ecoapp/Models/Message.java
+++ app/src/main/java/com/lumastech/ecoapp/Models/Message.java
@@ -3,5 +3,6 @@ package com.lumastech.ecoapp.Models;
 public class Message {
     public String created_at, message;
     public String sender_id;
+    public String receiver_id;
     public boolean isOpen;
 }
